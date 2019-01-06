2e0f066a42e90dc27bcac5b55b777a388ec34f3e
chenweiji <chenweijisun@126.com>
2019-01-06 11:45:55 +0800

    首页完成

--- app/src/main/java/com/kingcar/rent/pro/adapter/holder/CarMenuThreeBuilder.java
+++ app/src/main/java/com/kingcar/rent/pro/adapter/holder/CarMenuThreeBuilder.java
@@ -1,12 +1,15 @@
 package com.kingcar.rent.pro.adapter.holder;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.kingcar.rent.pro.R;
+import com.kingcar.rent.pro.widget.DashLineDecoration;
 
 public class CarMenuThreeBuilder extends BaseHolerBuilder<CarMenuThreeBuilder.ViewHolder> {
 
@@ -22,14 +25,49 @@ public class CarMenuThreeBuilder extends BaseHolerBuilder<CarMenuThreeBuilder.Vi
     public class ViewHolder extends RecyclerView.ViewHolder {
 
 
+        private RecyclerView recyclerView;
+
         public ViewHolder(View itemView) {
             super(itemView);
-
+            recyclerView=itemView.findViewById(R.id.recyclerView);
             init();
         }
 
-        private void init() {
+        private void init(){
+            LinearLayoutManager manager=new LinearLayoutManager(itemView.getContext());
+            manager.setOrientation(LinearLayoutManager.VERTICAL);
+            recyclerView.setLayoutManager(manager);
+            recyclerView.addItemDecoration(new DashLineDecoration());
+            recyclerView.setAdapter(new CarMenuThreeBuilder.CarMenuThreeAdapter(itemView.getContext()));
+        }
+    }
+    class CarMenuThreeAdapter extends RecyclerView.Adapter{
+        private Context context;
 
+        public CarMenuThreeAdapter(Context context) {
+            this.context = context;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_car_menu_three, parent, false);
+            return new RecyclerView.ViewHolder(view) {
+                @Override
+                public String toString() {
+                    return super.toString();
+                }
+            };
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 5;
         }
     }
 }
--- app/src/main/java/com/kingcar/rent/pro/adapter/holder/CarMenuTwoBuilder.java
+++ app/src/main/java/com/kingcar/rent/pro/adapter/holder/CarMenuTwoBuilder.java
@@ -1,12 +1,15 @@
 package com.kingcar.rent.pro.adapter.holder;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.kingcar.rent.pro.R;
+import com.kingcar.rent.pro.widget.DashLineDecoration;
 
 public class CarMenuTwoBuilder extends BaseHolerBuilder<CarMenuTwoBuilder.ViewHolder>{
 
@@ -21,15 +24,51 @@ public class CarMenuTwoBuilder extends BaseHolerBuilder<CarMenuTwoBuilder.ViewHo
 
     public  class ViewHolder extends RecyclerView.ViewHolder{
 
+        private RecyclerView recyclerView;
 
         public ViewHolder(View itemView) {
             super(itemView);
-
+            recyclerView=itemView.findViewById(R.id.recyclerView);
             init();
         }
 
         private void init(){
-
+            LinearLayoutManager manager=new LinearLayoutManager(itemView.getContext());
+            manager.setOrientation(LinearLayoutManager.VERTICAL);
+            recyclerView.setLayoutManager(manager);
+            recyclerView.addItemDecoration(new DashLineDecoration());
+            recyclerView.setAdapter(new CarMenuTwoAdapter(itemView.getContext()));
         }
     }
+
+    class CarMenuTwoAdapter extends RecyclerView.Adapter{
+        private Context context;
+
+        public CarMenuTwoAdapter(Context context) {
+            this.context = context;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_car_menu_two, parent, false);
+            return new RecyclerView.ViewHolder(view) {
+                @Override
+                public String toString() {
+                    return super.toString();
+                }
+            };
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
+    }
+
 }
